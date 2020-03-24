fc9820e1d16b7aa4eb11460457ac601cbeb65128
d-pilin <pilin.dmitriy.t@gmail.com>
2020-03-24 15:03:50 +0200

    Second Homework

---
+++ pilin_dmitriy/02/2_2.py
@@ -0,0 +1,41 @@
+#text = list(input('Input string to reverse: '))
+
+text=list('Hello')
+
+print('\nFirst example ')
+
+let_num = len(text) - 1
+
+while let_num > -1 :
+
+    print(text[let_num], end='')
+    let_num -= 1
+
+print('\nSecond example ')
+
+for i in range(len(text) - 1, -1, -1):
+    print(text[i],end='')
+
+print('\nThird example')
+     
+new_text = []
+
+for i in range(-1, -(len(text) + 1), -1):
+    new_text.append(text[i])
+
+for letter in new_text:
+    print(letter, end = '')
+
+
+# Not an example. Just a cheat sheet
+'''
+txt = "Hello World"[::-1]
+print(txt)
+'''
+
+
+
+
+    
+
+
---
+++ pilin_dmitriy/02/2_3.py
@@ -0,0 +1,36 @@
+#raw_list = list(input('Input raw int: '))
+
+raw_list = [1, 2, 3, 1, 3, 2, 1]
+
+clean_list = []
+
+for n in raw_list:
+
+    if [n,0] not in clean_list:
+        clean_list.append([n,0])
+
+for n in raw_list:
+
+    index = 0
+
+    for l in clean_list:
+
+        count = l[1]
+        number = l[0]
+
+        if n == number:
+            clean_list[index] = [number,count + 1]
+
+        index = index + 1
+
+for i in range(len(clean_list)):
+
+    if  clean_list[i][1] % 2:
+        print(f'The odd number is: {clean_list[i][0]}')
+
+#--or--
+
+result = [i for i in range(len(clean_list)) if clean_list[i][1] % 2]
+
+for i in result:
+    print(f'The odd number is: {clean_list[i][0]}')
